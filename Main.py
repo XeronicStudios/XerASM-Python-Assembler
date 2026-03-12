254cf2f5ba6b9dff5546420691939d3cf75eefdd
XeronicStudios <149962058+XeronicStudios@users.noreply.github.com>
2026-03-12 23:41:06 +0000

    First Edit, added a bit more to the code. Still not anywhere near done.

--- Main.py
+++ Main.py
@@ -1,11 +1,15 @@
-##Variables and Lists
+##Variables, Dictionaries and Lists
 Sections = []
+Operators = ["LDA", "STR", "ADD", "SUB", "MUL", "DIV", "MOV", "CMP", "JMP", "JEQ", "JNE", "JGT", "JLT", "AND", "ORR", "EOR", "NOT"]
+Errors = ["INVALID_INSTRUCTION", "FILE_NOT_FOUND", "INVALID_REGISTER", "INVALID OPERANDS FOR INSTRUCTION"]
 
 ##Classes
+#SectionData class used to store all relevant data on a section of xasm
 class SectionData:
-    SectionLabel = 0
-    SectionSize = 0
-    Instructions = []
+    def __init__(self, Label: str, Size: int, Data: list):
+        self.SectionLabel: str = Label
+        self.SectionSize: int = Size
+        self.Instructions = Data
 
 ##Functions
 #Gets the different sections from the xasm file
@@ -13,24 +17,50 @@ def GetSections(file: str):
 
     #Confirms the file exists
     try:
-        with open(file, 'r') as File:
-            print("FILE_EXISTS")
+        File = open(file, 'r')
+        File.close()
     except:
-        print("ERR_FILE_DOES_NOT_EXIST")
+        print(Errors[1])
         return
     
     #Creates a SectionData Object to store the section data
     with open(file, 'r') as File:
         Temp = SectionData
         TempList = []
+        Counter = 0
+        SecSize = 0
         for Line in File:
             #Removes the new line character
             if "\n" in Line:
                 Line = Line.replace("\n", "")
-            TempList.append(Line)
+            
+            if "    " in Line:
+                Line = Line.replace("   ", "")
+            if "    " in Line:
+                Line = Line.replace("    ", "")
+
+            try:
+                if Line[0] == "." and Counter == 0:
+                    Counter += 1
+                    Temp = SectionData
+                    TempList = []
+                elif Line[0] == "." and Counter != 0:
+                    Counter += 1
+                    Temp.Instructions = TempList
+                    Temp.SectionSize = SecSize
+                    Sections.append(Temp)
+                    SecSize = 0
+                    Temp = SectionData
+                    TempList = []
+                else:
+                    SecSize += 1
+                    TempList.append(Line)
+            except:
+                print("Empty Line")
         Temp.Instructions = TempList
         Sections.append(Temp)
 
 ##Main
 GetSections("XerASM.xasm")
-print(Sections[0].Instructions)
+print(Sections[1].Instructions)
+print(Sections[1].SectionSize)
